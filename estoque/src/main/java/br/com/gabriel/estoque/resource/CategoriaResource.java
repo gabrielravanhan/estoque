208073d4bcf660f39d61ecf1048a85b38d4fcedd
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2021-11-05 22:10:08 -0300

    .

--- estoque/src/main/java/br/com/gabriel/estoque/resource/CategoriaResource.java
+++ estoque/src/main/java/br/com/gabriel/estoque/resource/CategoriaResource.java
@@ -3,16 +3,20 @@ package br.com.gabriel.estoque.resource;
 import br.com.gabriel.estoque.model.Categoria;
 import br.com.gabriel.estoque.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/categorias")
 public class CategoriaResource {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+    @GetMapping()
     public List<Categoria> listarTodasCategorias() {
         return categoriaRepository.findAll();
     }
--- estoque/src/main/java/br/com/gabriel/estoque/resource/ProdutoResource.java
+++ estoque/src/main/java/br/com/gabriel/estoque/resource/ProdutoResource.java
@@ -3,16 +3,20 @@ package br.com.gabriel.estoque.resource;
 import br.com.gabriel.estoque.model.Produto;
 import br.com.gabriel.estoque.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/produtos")
 public class ProdutoResource {
 
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @GetMapping()
     public List<Produto> listarTodosProdutos() {
         return produtoRepository.findAll();
     }
